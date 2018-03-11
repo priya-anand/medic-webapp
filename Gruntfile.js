adae77279384ac9e524388627363c0a71140d062
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2018-03-12 12:26:45 +1300

    Try installing transitive dependencies first

--- Gruntfile.js
+++ Gruntfile.js
@@ -450,7 +450,15 @@ module.exports = function(grunt) {
     },
     auto_install: {
       npm: {
-        bower: false
+        options: {
+          bower: false
+        }
+      },
+      phone_number: {
+        options: {
+          bower: false,
+          cwd: 'shared-libs/phone-number'
+        }
       }
     },
     'regex-check': {
@@ -498,6 +506,7 @@ module.exports = function(grunt) {
   // Build tasks
   grunt.registerTask('mmnpm', 'Update and patch npm dependencies', [
     'exec:undopatches',
+    'auto_install:phone_number',
     'auto_install:npm',
     'copy:librariestopatch',
     'exec:applypatches'
